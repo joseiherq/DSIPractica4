91d464805730edfd8ece75bf7ebefea9f9d36717
joseiherq <joseiherq@gmail.com>
2013-03-12 12:28:46 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DSIPractica4::Application.config.secret_token = '0edbbdfe839af2b1ef75fbd1b7febe3347d9ef5c7a589564e3a7679ea30215b4b007d9c2c8562266b4e982cddd2fb00e80a796dd81d52a6d323f28f0f65bde4e'
